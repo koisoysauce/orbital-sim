774a6174038bf7bb524bc38abcbc6f1dd2ceb165
koisoysauce <kai.jp2014@gmail.com>
2025-12-14 23:58:52 -0600

    Attempt to add Runge Kutter 4th Order Integration

--- scr/integrators.py
+++ scr/integrators.py
@@ -5,6 +5,22 @@ import numpy as np
 # This method has a lot more trouble with ellipses that are not circles
 def euler_step(bodyL, bodyS, step):
     bodyS.position = bodyS.position + step * bodyS.velocity
-    bodyS.velocity = bodyS.velocity + step * get_accel(bodyL, bodyS)
+    bodyS.velocity = bodyS.velocity + step * get_accel(bodyL, bodyS.position)
 
-# Try implementing a different ODE solving method later like RK4
+# Implement RK4 Method
+# TODO: Fix RK4 Method, not perfect
+def RK4_step(bodyL, bodyS, step):
+    # Update the velocity
+    k1v = get_accel(bodyL, bodyS.position)
+    k2v = get_accel(bodyL, bodyS.position + step / 2 * k1v)
+    k3v = get_accel(bodyL, bodyS.position + step / 2 * k2v)
+    k4v = get_accel(bodyL, bodyS.position + step * k3v)
+    dv = step / 6 * (k1v + 2 * k2v + 2 * k3v + k4v)
+
+    # Update the position
+    k1x = bodyS.velocity
+    k2x = bodyS.velocity + 1 / 2 * dv
+    k3x = bodyS.velocity + 1 / 2 * dv
+    k4x = bodyS.velocity + 1 * dv
+    bodyS.position += step / 6 * (k1x + 2 * k2x + 2 * k3x + k4x)
+    bodyS.velocity += dv
--- scr/physics.py
+++ scr/physics.py
@@ -3,11 +3,11 @@
 from math import *
 import numpy as np
 
-def get_accel(bodyL, bodyS):
+def get_accel(bodyL, bodyS_pos):
     G = 6.674 * 10 ** -11
     mu = G * bodyL.mass
     
-    r = bodyS.position - bodyL.position
+    r = bodyS_pos - bodyL.position
     r_mag = np.linalg.norm(r) # just pythag theorem to get distance
 
     return - mu * r / r_mag ** 3
