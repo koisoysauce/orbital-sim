2074ad0f3954b4c6fb149a28a15d161a94d1dc6b
koisoysauce <kai.jp2014@gmail.com>
2025-12-15 19:32:00 -0600

    Move RK4 Explanation to README

--- scr/integrators.py
+++ scr/integrators.py
@@ -9,26 +9,6 @@ def euler_step(bodyL, bodyS, step):
 
 # Implement RK4 Method
 
-'''Runge Kutta 4th Order Method Explained'''
-
-# The Runge Kutta is an extension of the Euler's Method
-# in which Euler's step is the first order of the Runge Kutta Method.
-# This means that Runge Kutta is more likely to give a more accurate approximation 
-# of the next value compared to Euler's method
-
-# With this method, we essentially get the slope of the function at the starting point and then predict its first value, k1. (First-Order)
-# We then will get the midpoint of the linear equation found from first-order and will get the slope of the function at that point, k2. (Second-Order)
-# We then again get the midpoint of the linear equation found from second-order and gets the slope of the function at that point, k3. (Third-Order)
-# For the fourth-order, we get the slope of the function from the point that we found in third-order, k4.
-
-# We can then use these slopes to approximate the final point using the average
-# Each order that uses a half-step has a weight of 1/3
-# The orders that don't have a weight of 1/6
-# After we get the average, we will multiply this by the step and add to the initial value to get the approximated value.
-
-# The final equation will look like so:
-# x_{n + 1} = x_{n} + step / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
-
 def RK4_step(bodyL, bodyS, step):
     # k1
     k1x = bodyS.velocity
