ef8af9ac7600bf79a6411f88f14f7e994c28e9c2
koisoysauce <kai.jp2014@gmail.com>
2025-12-17 13:52:37 -0600

    Apply gravitational force to both bodies

--- scr/physics.py
+++ scr/physics.py
@@ -3,23 +3,23 @@
 from math import *
 import numpy as np
 
-def get_accel(bodyL, bodyS_pos):
-    G = 6.674 * 10 ** -11
-    mu = G * bodyL.mass
+# def get_accel(bodyL, bodyS_pos):
+#     G = 6.674 * 10 ** -11
+#     mu = G * bodyL.mass
     
-    r = bodyS_pos - bodyL.position
-    r_mag = np.linalg.norm(r) # just pythag theorem to get distance
+#     r = bodyS_pos - bodyL.position
+#     r_mag = np.linalg.norm(r) # just pythag theorem to get distance
 
-    return - mu * r / r_mag ** 3
+#     return - mu * r / r_mag ** 3
 
 # TODO: use body2_pos as a checker if there is addition to it with the use of the RK4 method
-def get_accel(body1, body2, body2_pos = ''):
+def get_accel(body1, body2):
     G = 6.674e-11
-    r = body1.position - body2.position # relative to body1
+    r = body2.position - body1.position # relative to body1
     # r_mag = sqrt((body1.position[0] - body2.position[0]) ** 2 + (body1.position[1] - body2.position[1]) ** 2)
     r_mag = np.linalg.norm(r) # This is more efficient and accurate
 
     a1 = G * body2.mass * r / r_mag ** 3 # Should be towards body2
-    a2 = G * body1.mass * (-r) / r_mag ** 3 # Should be towards body1
+    # a2 = G * body1.mass * (-r) / r_mag ** 3 # Should be towards body1
 
-    return a1, a2
+    return a1
--- scr/simulate.py
+++ scr/simulate.py
@@ -7,8 +7,15 @@ plt.style.use("dark_background")
 
 # We need two bodies, one representing a planet and the other representing an object in orbit
 
-planet = Body("Earth", 5.97 * 10 ** 24, 6.371 * 10 ** 6, np.array([0, 0]), np.array([0, 0]))
-spacecraft = Body("Spacecraft", 100, 50, np.array([7e6, 0.0]), np.array([0.0, 7800.0]))
+# Earth mass: 5.97e24kg; radius: 6.371e6km
+
+'''Equal mass'''
+# body1 = Body("Earth", 5.97e24, 6.371e6, np.array([0, 0]), np.array([0, -7800.0]))
+# body2 = Body("Earth", 5.97e24, 6.371e6, np.array([5e6, 0.0]), np.array([0.0, 7800.0]))
+
+'''Earth and satellite'''
+body1 = Body("Earth", 5.97e24, 6.371e6, np.array([0, 0]), np.array([0, 0]))
+body2 = Body("Satellite", 100, 50, np.array([2e6, 0]), np.array([0, 7800.0]))
 
 # Now we can graph the positions of the spacecraft with respect to Earth
 
@@ -18,32 +25,39 @@ plt.title("2 Body Orbital")
 plt.xlim(-1e8, 1e8)
 plt.ylim(-1e7, 1e7)
 
-# We need to get an array of the x and y positions
+# Plot both bodies as undecided to animate it's movement
 
-x_pos = [spacecraft.position[0]]
-y_pos = [spacecraft.position[1]]
+body1_plot, = plt.plot([], [], 'bo')
+body1_trail, = plt.plot([], [], 'b-', alpha=0.5)
+body2_plot, = plt.plot([], [], 'ro')
+body2_trail, = plt.plot([], [], 'r-', alpha=0.5)
 
-# Plot the earth as a dot and craft as undecided as we will animate it's movement
+body1_x_trail = []
+body1_y_trail = []
+body2_x_trail = []
+body2_y_trail = []
 
-plt.plot(0, 0, 'bo')
-craft_plot, = plt.plot([], [], 'ro') # The position of the craft
-trail_plot, = plt.plot([], [], 'r-', alpha = 0.5) # The trail that the craft leaves
-x_trail = []
-y_trail = []
+# plt.plot(0, 0, 'bo')
+# craft_plot, = plt.plot([], [], 'ro') # The position of the craft
+# trail_plot, = plt.plot([], [], 'r-', alpha = 0.5) # The trail that the craft leaves
+# x_trail = []
+# y_trail = []
 
-tstep = 50
+tstep = 1
 # while abs(x_pos[0] - x_pos[-1]) > 0.01 and abs(y_pos[0] - y_pos[-1]) > 0.01: # Fix this so that it accounts for only one element
-for i in range(5000):
-    RK4_step(planet, spacecraft, tstep)
+while True:
+    euler_step(body1, body2, tstep)
     
     # Now we will work on the plotting
 
-    x_trail.append(spacecraft.position[0])
-    y_trail.append(spacecraft.position[1])
+    body1_x_trail.append(body1.position[0])
+    body1_y_trail.append(body1.position[1])
+    body2_x_trail.append(body2.position[0])
+    body2_y_trail.append(body2.position[1])
 
-    craft_plot.set_data([spacecraft.position[0]], [spacecraft.position[1]])
-    trail_plot.set_data(x_trail, y_trail)
+    body1_plot.set_data([body1.position[0]], [body1.position[1]])
+    body1_trail.set_data(body1_x_trail, body1_y_trail)
+    body2_plot.set_data([body2.position[0]], [body2.position[1]])
+    body2_trail.set_data(body2_x_trail, body2_y_trail)
 
     plt.pause(0.001) # Pauses each frame for 0.001 seconds
-
-plt.show()
