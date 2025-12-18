eda077d3f1577facbfb867dc59ea9de2a5929331
koisoysauce <kai.jp2014@gmail.com>
2025-12-18 01:57:08 -0600

    Clean up comments

--- scr/simulate.py
+++ scr/simulate.py
@@ -5,11 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("dark_background")
 
-# We need two bodies, one representing a planet and the other representing an object in orbit
-
-# Earth mass: 5.97e24kg; radius: 6.371e6km
+# We need two bodies
 
 '''Equal mass'''
+# Earth mass: 5.97e24kg; radius: 6.371e6km
 body1 = Body("Earth", 5.97e24, 6.371e6, np.array([0, 0]), np.array([0, -7800.0]))
 body2 = Body("Earth", 5.97e24, 6.371e6, np.array([5e6, 0.0]), np.array([0.0, 7800.0]))
 
@@ -37,14 +36,8 @@ body1_y_trail = []
 body2_x_trail = []
 body2_y_trail = []
 
-# plt.plot(0, 0, 'bo')
-# craft_plot, = plt.plot([], [], 'ro') # The position of the craft
-# trail_plot, = plt.plot([], [], 'r-', alpha = 0.5) # The trail that the craft leaves
-# x_trail = []
-# y_trail = []
-
 tstep = 50
-# while abs(x_pos[0] - x_pos[-1]) > 0.01 and abs(y_pos[0] - y_pos[-1]) > 0.01: # Fix this so that it accounts for only one element
+
 while True:
     RK4_step(body1, body2, tstep)
     
