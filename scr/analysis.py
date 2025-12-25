26931f4dc6d915523860715ed8d1173d97b0265a
koisoysauce <kai.jp2014@gmail.com>
2025-12-25 17:09:23 -0600

    Implement Angular Momentum Error vs. Timestep graph

--- scr/analysis.py
+++ scr/analysis.py
@@ -6,52 +6,16 @@ from integrators import *
 from physics import *
 plt.style.use("dark_background")
 
-# Functions calculate y-values by step
-# TODO: fix calculations because they are probably wrong
-def total_energy_error(bodies):
-    positions = np.array([body.position for body in bodies])
-    velocities = np.array([body.velocity for body in bodies])
-    masses = np.array([body.mass for body in bodies])
-    G = 6.674e-11 # Gravitational Constant
-
-    E = 0
-    # # Get total kinetic energy
-    # E += np.sum(0.5 * masses * velocities ** 2)
-    # Get total gravitational energy
-    for i in range(len(bodies)):
-        E += 0.5 * masses[i] * np.linalg.norm(velocities[i]) ** 2
-        for n in range(len(bodies)):
-            if i != n:
-                r = np.linalg.norm(positions[i] - positions[n])
-                E -= G * masses[i] * masses[n] / r
-    
-    return E
-
-def angular_momentum_error(bodies):
-    positions = np.array([body.position for body in bodies])
-    velocities = np.array([body.velocity for body in bodies])
-    masses = np.array([body.mass for body in bodies])
-
-    L = 0
-
-    for i in range(len(bodies)):
-        for n in range(len(bodies)):
-            if i != n:
-                r = positions[i] - positions[n]
-                L += r * masses[i] * velocities[i]
-
-    return L
-
-# TODO: finish other functions and add more plots to energy error, then add momentum error plot
-def update(frame):
+'''Energy plot'''
+def update_energy(frame):
     euler_step(temp_bodies_euler, timestep)
     RK4_step(temp_bodies_RK4, timestep)
     vel_verlet_step(temp_bodies_verlet, timestep)
 
     # Add new values
-    energy_error_euler.append(total_energy_error(temp_bodies_euler) - E0)
-    energy_error_RK4.append(total_energy_error(temp_bodies_RK4) - E0)
-    energy_error_verlet.append(total_energy_error(temp_bodies_verlet) - E0)
+    energy_error_euler.append(total_energy(temp_bodies_euler) - E0)
+    energy_error_RK4.append(total_energy(temp_bodies_RK4) - E0)
+    energy_error_verlet.append(total_energy(temp_bodies_verlet) - E0)
     timesteps.append(frame + 1)
     
     # Update properties
@@ -59,26 +23,26 @@ def update(frame):
     energy_error_RK4_plot.set_data(timesteps, energy_error_RK4)
     energy_error_verlet_plot.set_data(timesteps, energy_error_verlet)
 
-    for i in range(len(axs)):
-        axs[i].relim()
-        axs[i].autoscale_view()
+    for i in range(len(axs_energy)):
+        axs_energy[i].relim()
+        axs_energy[i].autoscale_view()
     
     return [energy_error_euler_plot, energy_error_RK4_plot, energy_error_verlet_plot]
 
 # Graph Total Energy Error vs Timestep
 
-fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True)
-fig.suptitle("Total Energy Error vs. Timestep")
-axs[0].set_title("Euler")
-axs[1].set_title("Runge Kutta 4")
-axs[2].set_title("Velocity Verlet")
-axs[0].set_ylabel("Energy Error (J)")
-axs[1].set_ylabel("Energy Error (J)")
-axs[2].set_xlabel("timestep")
-axs[2].set_ylabel("Energy Error (J)")
+fig_energy, axs_energy = plt.subplots(nrows=3, ncols=1, sharex=True)
+fig_energy.suptitle("Total Energy Error vs. Timestep")
+axs_energy[0].set_title("Symplectic Euler")
+axs_energy[1].set_title("Runge Kutta 4")
+axs_energy[2].set_title("Velocity Verlet")
+axs_energy[0].set_ylabel("Energy Error (J)")
+axs_energy[1].set_ylabel("Energy Error (J)")
+axs_energy[2].set_xlabel("timestep")
+axs_energy[2].set_ylabel("Energy Error (J)")
 
 # Get initial total energy
-E0 = total_energy_error(bodies)
+E0 = total_energy(bodies)
 
 # Get static plot
 
@@ -91,11 +55,65 @@ energy_error_euler = [0]
 energy_error_RK4 = [0]
 energy_error_verlet = [0]
 
-energy_error_euler_plot, = axs[0].plot(timesteps, energy_error_euler, 'r-', label='euler')
-energy_error_RK4_plot, = axs[1].plot(timesteps, energy_error_RK4, 'g-', label='RK4')
-energy_error_verlet_plot, = axs[2].plot(timesteps, energy_error_verlet, 'b-', label='verlet')
+energy_error_euler_plot, = axs_energy[0].plot(timesteps, energy_error_euler, 'r-', label='euler')
+energy_error_RK4_plot, = axs_energy[1].plot(timesteps, energy_error_RK4, 'g-', label='RK4')
+energy_error_verlet_plot, = axs_energy[2].plot(timesteps, energy_error_verlet, 'b-', label='verlet')
 
 # Initiate animation
-ani = animation.FuncAnimation(fig, update, interval=20, blit=False, cache_frame_data=False)
+ani_energy = animation.FuncAnimation(fig_energy, update_energy, interval=20, blit=False, cache_frame_data=False)
+
+plt.show()
+
+'''Angular Momentum Plot'''
+def update_momentum(frame):
+    euler_step(temp_bodies_euler, timestep)
+    RK4_step(temp_bodies_RK4, timestep)
+    vel_verlet_step(temp_bodies_verlet, timestep)
+
+    timesteps.append(frame + 1)
+    momentum_error_euler.append(total_angular_momentum(temp_bodies_euler) - L0)
+    momentum_error_RK4.append(total_angular_momentum(temp_bodies_RK4) - L0)
+    momentum_error_verlet.append(total_angular_momentum(temp_bodies_verlet) - L0)
+
+    # Update properties
+    momentum_error_euler_plot.set_data(timesteps, momentum_error_euler)
+    momentum_error_RK4_plot.set_data(timesteps, momentum_error_RK4)
+    momentum_error_verlet_plot.set_data(timesteps, momentum_error_verlet)
+
+    for i in range(len(axs_momentum)):
+        axs_momentum[i].relim()
+        axs_momentum[i].autoscale_view()
+
+    return [momentum_error_euler_plot, momentum_error_RK4_plot, momentum_error_verlet_plot]
+
+# Graph Total Angular Momentum vs. timestep
+fig_momentum, axs_momentum = plt.subplots(nrows=3, ncols=1, sharex=True)
+fig_momentum.suptitle("Total Angular Momentum Error vs. Timestep")
+axs_momentum[0].set_title("Symplectic Euler")
+axs_momentum[1].set_title("Runge Kutta 4")
+axs_momentum[2].set_title("Velocity Verlet")
+axs_momentum[1].set_ylabel("Angular Momentum Error (kgm²/s)")
+axs_momentum[2].set_xlabel("timestep")
+
+# Get initial angular momentum
+L0 = total_angular_momentum(bodies)
+
+# Get static plot
+
+temp_bodies_euler = [body.copy() for body in bodies]
+temp_bodies_RK4 = [body.copy() for body in bodies]
+temp_bodies_verlet = [body.copy() for body in bodies]
+
+timesteps = [0]
+momentum_error_euler = [0]
+momentum_error_RK4 = [0]
+momentum_error_verlet = [0]
+
+momentum_error_euler_plot, = axs_momentum[0].plot(timesteps, momentum_error_euler, 'r-', label='euler')
+momentum_error_RK4_plot, = axs_momentum[1].plot(timesteps, momentum_error_RK4, 'g-', label='RK4')
+momentum_error_verlet_plot, = axs_momentum[2].plot(timesteps, momentum_error_verlet, 'b-', label='verlet')
+
+# Initiate animation
+ani_momentum = animation.FuncAnimation(fig_momentum, update_momentum, interval=20, blit=False, cache_frame_data=False)
 
 plt.show()
--- scr/physics.py
+++ scr/physics.py
@@ -21,3 +21,41 @@ def get_accels(positions, masses):
         accels[i] = total
         total = 0
     return accels
+
+def total_energy(bodies):
+    positions = np.array([body.position for body in bodies])
+    velocities = np.array([body.velocity for body in bodies])
+    masses = np.array([body.mass for body in bodies])
+    G = 6.674e-11 # Gravitational Constant
+
+    K = 0
+    U = 0
+    for i in range(len(bodies)):
+        K += 0.5 * masses[i] * np.linalg.norm(velocities[i]) ** 2
+        for n in range(len(bodies)):
+            if i < n:
+                r = positions[i] - positions[n]
+                r_mag = np.linalg.norm(r)
+
+                U += -G * masses[i] * masses[n] / r_mag
+    E = K + U
+    
+    return E
+
+# angular momentum in magnitude for the momentum vs. time graph
+# Assume all bodies are rotating about the center (0, 0, 0)
+def total_angular_momentum(bodies):
+    positions = np.array([body.position for body in bodies])
+    velocities = np.array([body.velocity for body in bodies])
+    masses = np.array([body.mass for body in bodies])
+
+    L = 0
+    for i in range(len(bodies)):
+        L += np.cross(positions[i], masses[i] * velocities[i])
+
+    return np.linalg.norm(L)
+# def total_angular_momentum(bodies):
+#     L_total = np.array([0.0, 0.0, 0.0])  # start as 3D vector
+#     for body in bodies:
+#         L_total += np.cross(body.position, body.velocity * body.mass)
+#     return np.linalg.norm(L_total)
